bd3cd9ebb4d254ffb841f0b6fd9b8f9171d18fcd
Leman-Quinn <PabloN.A@hotmail.com>
2025-03-28 01:48:51 -0300

    Se agrego groupbox Estado Civil y sus opciones

--- TrabajoPractico1/Ejercicio3.Designer.cs
+++ TrabajoPractico1/Ejercicio3.Designer.cs
@@ -30,9 +30,13 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Ejercicio3));
             this.gboxSexo = new System.Windows.Forms.GroupBox();
-            this.rbFemenino = new System.Windows.Forms.RadioButton();
             this.rbMasculino = new System.Windows.Forms.RadioButton();
+            this.rbFemenino = new System.Windows.Forms.RadioButton();
+            this.gboxEstadoCivil = new System.Windows.Forms.GroupBox();
+            this.rbCasado = new System.Windows.Forms.RadioButton();
+            this.rbSoltero = new System.Windows.Forms.RadioButton();
             this.gboxSexo.SuspendLayout();
+            this.gboxEstadoCivil.SuspendLayout();
             this.SuspendLayout();
             // 
             // gboxSexo
@@ -49,6 +53,16 @@
             this.gboxSexo.TabStop = false;
             this.gboxSexo.Text = "Sexo";
             // 
+            // rbMasculino
+            // 
+            this.rbMasculino.AutoSize = true;
+            this.rbMasculino.Location = new System.Drawing.Point(6, 42);
+            this.rbMasculino.Name = "rbMasculino";
+            this.rbMasculino.Size = new System.Drawing.Size(86, 20);
+            this.rbMasculino.TabIndex = 1;
+            this.rbMasculino.Text = "Masculino";
+            this.rbMasculino.UseVisualStyleBackColor = true;
+            // 
             // rbFemenino
             // 
             this.rbFemenino.AutoSize = true;
@@ -61,21 +75,46 @@
             this.rbFemenino.Text = "Femenino";
             this.rbFemenino.UseVisualStyleBackColor = true;
             // 
-            // rbMasculino
+            // gboxEstadoCivil
             // 
-            this.rbMasculino.AutoSize = true;
-            this.rbMasculino.Location = new System.Drawing.Point(6, 42);
-            this.rbMasculino.Name = "rbMasculino";
-            this.rbMasculino.Size = new System.Drawing.Size(86, 20);
-            this.rbMasculino.TabIndex = 1;
-            this.rbMasculino.Text = "Masculino";
-            this.rbMasculino.UseVisualStyleBackColor = true;
+            this.gboxEstadoCivil.Controls.Add(this.rbSoltero);
+            this.gboxEstadoCivil.Controls.Add(this.rbCasado);
+            this.gboxEstadoCivil.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gboxEstadoCivil.Location = new System.Drawing.Point(318, 34);
+            this.gboxEstadoCivil.Name = "gboxEstadoCivil";
+            this.gboxEstadoCivil.Size = new System.Drawing.Size(104, 83);
+            this.gboxEstadoCivil.TabIndex = 1;
+            this.gboxEstadoCivil.TabStop = false;
+            this.gboxEstadoCivil.Text = "Estado Civil";
+            // 
+            // rbCasado
+            // 
+            this.rbCasado.AutoSize = true;
+            this.rbCasado.Checked = true;
+            this.rbCasado.Location = new System.Drawing.Point(6, 19);
+            this.rbCasado.Name = "rbCasado";
+            this.rbCasado.Size = new System.Drawing.Size(73, 20);
+            this.rbCasado.TabIndex = 0;
+            this.rbCasado.TabStop = true;
+            this.rbCasado.Text = "Casado";
+            this.rbCasado.UseVisualStyleBackColor = true;
+            // 
+            // rbSoltero
+            // 
+            this.rbSoltero.AutoSize = true;
+            this.rbSoltero.Location = new System.Drawing.Point(6, 42);
+            this.rbSoltero.Name = "rbSoltero";
+            this.rbSoltero.Size = new System.Drawing.Size(68, 20);
+            this.rbSoltero.TabIndex = 1;
+            this.rbSoltero.Text = "Soltero";
+            this.rbSoltero.UseVisualStyleBackColor = true;
             // 
             // Ejercicio3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(659, 450);
+            this.ClientSize = new System.Drawing.Size(472, 450);
+            this.Controls.Add(this.gboxEstadoCivil);
             this.Controls.Add(this.gboxSexo);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Ejercicio3";
@@ -84,6 +123,8 @@
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Ejercicio3_FormClosed);
             this.gboxSexo.ResumeLayout(false);
             this.gboxSexo.PerformLayout();
+            this.gboxEstadoCivil.ResumeLayout(false);
+            this.gboxEstadoCivil.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -94,5 +135,8 @@
         private System.Windows.Forms.GroupBox gboxSexo;
         private System.Windows.Forms.RadioButton rbMasculino;
         private System.Windows.Forms.RadioButton rbFemenino;
+        private System.Windows.Forms.GroupBox gboxEstadoCivil;
+        private System.Windows.Forms.RadioButton rbSoltero;
+        private System.Windows.Forms.RadioButton rbCasado;
     }
 }
